7c1730bb4294f02a236f39769422fca25a0b77f1
YvesColletIdec <yves.collet.idec@gmail.com>
2026-03-30 18:17:09 +0200

    premier commit

---
+++ FactureEntities/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FactureEntities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
