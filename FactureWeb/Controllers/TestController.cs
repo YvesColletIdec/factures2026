dc4a0f5c109b83dee9a9b70812a725f51094c5a6
YvesColletIdec <yves.collet.idec@gmail.com>
2026-04-20 21:13:59 +0200

    ajout projet web

---
+++ FactureWeb/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FactureWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Coucou()
+        {
+            return View();
+        }
+
+        public IActionResult Affichage(string prenom)
+        {
+            string resultat = $"salut {prenom}";
+            ViewBag.mavaleur = resultat;
+            return View();
+        }
+    }
+}
---
+++ FactureWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FactureWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
